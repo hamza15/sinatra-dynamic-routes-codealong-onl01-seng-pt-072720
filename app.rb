9f0ad89def0cc2728d5c243297b840c66b08fac0
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-21 05:42:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,5 +18,10 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Goodbye, #{@user_name}."
   end
+  
+  get "/multiply/:num1/:num2" do
+    @result = params[:num1].to_i * params[:num2].to_i
+    "#{@result}"
+  end
 
 end
